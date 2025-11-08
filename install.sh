a41b8d60f2f62ec14b36384780c5ed0b48ef5841
Jan Brezina <brezinajn@users.noreply.github.com>
2025-11-08 13:56:24 +0100

    Update script download url to point to this repo.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o /tmp/tas2781-fix.sh  https://raw.githubusercontent.com/DanielWeiner/tas2781-fix-16IRX8H/refs/heads/main/tas2781-fix.sh
+curl -o /tmp/tas2781-fix.sh  https://raw.githubusercontent.com/brezinajn/tas2781-fix-16ARX8H/refs/heads/main/tas2781-fix.sh
 chmod +x /tmp/tas2781-fix.sh
 /tmp/tas2781-fix.sh --install
 rm /tmp/tas2781-fix.sh
